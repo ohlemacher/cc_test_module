4a81f407eb7b9dc4a4f1ea3e2ca4dcc1de539fbf
David Ohlemacher <ohlemacher@us.ibm.com>
2017-12-31 14:29:13 -0500

    Add UT bash script

---
+++ test_module.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o pipefail
+set -o errexit
+
+if [ "$(hostname)" == "pluto.local" ]; then
+    echo "Configured for pluto execution"
+    declare -r cc_test_module_dir="cc_test_module"
+else
+    declare -r cc_test_module_dir="cc-test-module-repo"
+fi
+
+function info {
+    local msg="$1"
+    (>&2 echo Info: "$msg")  # Subshell avoids interactions with other redirections
+}
+
+function die {
+    local msg="$1"
+    (>&2 echo Fatal: "$msg")  # Subshell avoids interactions with other redirections
+    exit 1
+}
+
+function install_prereqs {
+    pip install pytest || die "pip install pytest failed"
+}
+
+function run_unit_tests {
+    info "Run unit tests for cc_test_test_module"
+    pushd "$cc_test_module_dir" > /dev/null
+    python -m pytest -v test_module.py || die "cc_test_test_module unit tests failed"
+    popd > /dev/null
+}
+
+install_prereqs
+run_unit_tests
+
