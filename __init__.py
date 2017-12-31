8512cba31e96da933acc26e65a89f8308af2da18
David Ohlemacher <ohlemacher@us.ibm.com>
2017-12-31 09:46:38 -0500

    Add UTs for cc_test_module.py.

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# __all__ = ["cc_test_module"]
-
-from cc_test_module.cc_test_module import rot13_encode
-
-
--- cc_test_module/cc_test_module.py
+++ cc_test_module/cc_test_module.py
@@ -1,6 +1,6 @@
-
 import codecs
 
 def rot13_encode(ins):
+    """ A trivial function to test in a package from concourse """
     out = codecs.encode(ins, 'rot_13')
     return out
