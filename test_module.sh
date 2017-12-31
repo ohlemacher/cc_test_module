a0d355c134cac1b31227088b7f89cf4f59613de8
David Ohlemacher <ohlemacher@us.ibm.com>
2017-12-31 18:48:32 -0500

    Trivial change to trigger cc.

--- test_module.sh
+++ test_module.sh
@@ -12,16 +12,17 @@ fi
 
 function info {
     local msg="$1"
-    (>&2 echo Info: "$msg")  # Subshell avoids interactions with other redirections
+    (>&2 echo "\n=== Info: $msg")  # Subshell avoids interactions with other redirections
 }
 
 function die {
     local msg="$1"
-    (>&2 echo Fatal: "$msg")  # Subshell avoids interactions with other redirections
+    (>&2 echo "\n!!! Fatal: $msg")  # Subshell avoids interactions with other redirections
     exit 1
 }
 
 function install_prereqs {
+    info "Install prereqs"
     pip install pytest || die "pip install pytest failed"
 }
 
